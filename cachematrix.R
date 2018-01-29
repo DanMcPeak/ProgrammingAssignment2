81b62d1d3ba79b8cfe27a499c41e4e54cd3441c3
Dan McPeak <mcpe0012@gmail.com>
2018-01-29 09:35:45 -0600

    Functionality provided within makeCacheMatrix and cacheSolve
    Function testing not completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i_X <- NULL
+  set <- function(y) {
+    x <<- y
+    i_x <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv_x) i_x <<- inv_x
+  getinv <- function() i_x
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv() ##grab current inverse value
+  
+  ##If the inverse matrix is available, send the cached info
+  if(!is.null(inv)){
+    message("getting cached inv")
+    return(inv)
+  }
+  ##If not cached, grab the matrix
+  matrix <- x$get()
+  inv <- ginv(x)
+  x$setinv(inv)
+  inv
 }
